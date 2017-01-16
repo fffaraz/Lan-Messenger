ca08cdc3437740411148fe6b1c373dc6e56a1322
Sebastian Martin Dicke <Sebastianmartindicke@gmx.de>
2017-01-16 01:45:48 +0100

    Changed copyright notice.

--- Lan-Messenger/dialognickname.cpp
+++ Lan-Messenger/dialognickname.cpp
@@ -1,5 +1,6 @@
 // Lan Messenger
 // Copyright (C) 2012 Faraz Fallahi <fffaraz@gmail.com>
+// Copyright (C) 2017 Sebastian Martin Dicke <Sebastianmartindicke@gmx.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
